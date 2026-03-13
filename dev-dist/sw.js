159c0669f1f8e7676c2c393fb0ee6e7d6e2f5c80
Taha Khatip <tahakhatip2@gmail.com>
2026-03-13 15:02:39 +0300

    fix(ui): update patient chat bubbles and orientation

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f7c39696'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.oere85buom8"
+    "revision": "0.tih82tt50sc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
