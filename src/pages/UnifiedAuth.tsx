86fc60e9f1f387d018e68d72396b4bd6a2340ecf
Taha Khatip <tahakhatip2@gmail.com>
2026-05-29 23:22:05 +0300

    style: stretch auth background to exactly fit the screen

--- src/pages/UnifiedAuth.tsx
+++ src/pages/UnifiedAuth.tsx
@@ -257,7 +257,7 @@ const UnifiedAuth = () => {
 
             {/* Animated Background */}
             <div
-                className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+                className="fixed inset-0 bg-[length:100%_100%] bg-center bg-no-repeat"
                 style={{ backgroundImage: 'url(/doctor-jo-bg.png)' }}
             >
                 <div className="absolute inset-0 bg-[url('https://www.transparenttextures.com/patterns/carbon-fibre.png')] opacity-[0.03] mix-blend-overlay"></div>
